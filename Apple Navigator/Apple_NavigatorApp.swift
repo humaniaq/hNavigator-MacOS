919f2d1b569d2e48ba64988e668c96424459560c
Humaniaq <humaniaq.com@gmail.com>
2026-06-11 08:44:57 +0200

    Initial Commit

---
+++ Apple Navigator/Apple_NavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_NavigatorApp.swift
+//  Apple Navigator
+//
+//  Created by Leanid Losich on 11/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Apple_NavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Navigator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple Navigator
+//
+//  Created by Leanid Losich on 11/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
